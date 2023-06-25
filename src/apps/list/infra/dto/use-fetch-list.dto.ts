e1ee7062a125be5b276450a0c3a9d4f70d4416ad
sshim9619 <sshim9619@naver.com>
2023-06-26 08:30:03 +0900

    add:

---
+++ src/apps/list/infra/dto/use-fetch-list.dto.ts
@@ -0,0 +1,70 @@
+export type ListFetchResponses = {
+    results: Result[];
+    
+}
+
+export type Result = {
+
+}
+
+// {
+//     "results": [
+//         {
+//            
+//             "location": {
+//                 "street": {
+//                     "number": 5081,
+                    "name": "Atatürk Sk"
+//                 },
+//                 "city": "Mardin",
+//                 "state": "Antalya",
+//                 "country": "Turkey",
+//                 "postcode": 44888,
+//                 "coordinates": {
+//                     "latitude": "69.8597",
+//                     "longitude": "55.2812"
+//                 },
+//                 "timezone": {
+//                     "offset": "-6:00",
+//                     "description": "Central Time (US & Canada), Mexico City"
+//                 }
+//             },
+            "email": "elif.balci@example.com",
+//             "login": {
+//                 "uuid": "53b38091-674c-4b18-b12a-25a587b9a30b",
+//                 "username": "purpledog393",
+//                 "password": "beaver",
+//                 "salt": "DrWSfG0g",
+//                 "md5": "d51e186237f2727d928c883097aafee6",
+//                 "sha1": "f812752e001db97cd4a45215f191561bb1338881",
+//                 "sha256": "d3e8a1d3c0885c005a6bd47190c43f9725e7d22eb7c274129ee828936c8ba9c3"
+//             },
+//             "dob": {
+//                 "date": "1980-08-09T01:34:46.597Z",
+//                 "age": 42
+//             },
+//             "registered": {
+//                 "date": "2009-01-02T02:42:44.172Z",
+//                 "age": 14
+//             },
+            "phone": "(476)-620-7855",
+//             "cell": "(112)-403-4311",
+//             "id": {
+//                 "name": "",
+//                 "value": null
+//             },
+//             "picture": {
+//                 "large": "https://randomuser.me/api/portraits/women/51.jpg",
+//                 "medium": "https://randomuser.me/api/portraits/med/women/51.jpg",
+//                 "thumbnail": "https://randomuser.me/api/portraits/thumb/women/51.jpg"
+//             },
+//             "nat": "TR"
+//         }
+//     ],
+//     "info": {
+//         "seed": "376a67598543aabc",
+//         "results": 1,
+//         "page": 1,
+//         "version": "1.4"
+//     }
+// };
